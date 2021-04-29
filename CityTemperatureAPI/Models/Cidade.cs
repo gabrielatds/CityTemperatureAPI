f54ab45d6ee02b131e9d44a98fdde29126edbf48
Gabriel Souza <gabriel.souza@targetwork.com.br>
2021-04-29 19:46:45 -0300

    Conexão com banco de dados e Model Cidade

---
+++ CityTemperatureAPI/Models/Cidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityTemperatureAPI.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+        public  string Nome { get; set; }
+        public decimal TempAtual { get; set; }
+        public decimal TempMax { get; set; }
+        public decimal TempMin { get; set; }
+    }
+}
